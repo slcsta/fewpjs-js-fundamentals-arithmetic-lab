1922728da20b7f4f30ee930263d04f46631f5b27
Sarah <sarahlchavez@gmail.com>
2021-09-19 05:25:06 -0700

    commit tree 23cea452bbac4fc23f81df49b62a125836aacbbe

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() > 0);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 20);
+max;
